c2bacf3c7a9881550addb518f402e8eeeb2498aa
omkar.dalvi <omkar.dalvi@fractal.ai>
2025-05-10 16:25:59 +0000

    Added bootstrap files and added ruff and uv

---
+++ logistic_optimization/ml/features/build_features.py
@@ -0,0 +1,24 @@
+import click
+from dotenv import find_dotenv, load_dotenv
+from loguru import logger
+
+
+def pipeline():
+    logger.info("Start building features.")
+
+
+@click.command()
+@click.argument("input_filepath", default="data/interim", type=click.Path(exists=True))
+@click.argument("output_filepath", default="data/processed", type=click.Path())
+def main(input_filepath, output_filepath):
+    """Runs data processing scripts to turn cleaned data from (../interim) into
+    training data ready to be trained (saved in ../processed).
+    """
+    logger.info(f"Read from {input_filepath}, write to {output_filepath}.")
+
+
+if __name__ == "__main__":
+    load_dotenv(find_dotenv())
+
+    # pylint: disable = no-value-for-paramete
+    main()
